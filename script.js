45d32cd0ec602d747381e9ae4583bf97a18eb37c
tjakra <cakrasatria01@gmail.com>
2023-07-29 16:38:56 +0700

    update interface

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+var text = document.querySelector(".text")
+var isi = "HELLO, MY NAME IS CAKRA SATRIA PUTRA!"
+var isi2 = "I AM A WEB DEVELOPER"
+var next =false
+var i = 0
+var a = 0
+var b = 0
+
+setInterval(function(){
+  if(next){
+    typing(isi2,false)
+    console.log(next)
+  }
+  else{
+    typing(isi, true)
+  }
+}, 150)
+
+function typing(value, kondisi){
+  if(i< value.length){
+    text.innerHTML += value.charAt(i)
+    i++
+  }
+  else if(i == value.length){
+    var hapus = value.substring(0 , value.length- a)
+    text.innerHTML = hapus
+    b++ 
+    if(b > 10){
+      a++
+    }
+    if(a == value.length + 1){
+      i += 1
+    }
+  }
+  else if(a == value.length +1){
+    i = 0
+    a= 0
+    b=0
+    next = kondisi
+  }
+}
+
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
